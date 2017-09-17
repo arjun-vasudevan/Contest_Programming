6169b35b93bc49da048a35cb2bd6f0889876e355
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-17 09:24:54 -0400

    Delete AIF Essay.py

--- AIF Essay.py
+++
@@ -1,5 +0,0 @@
-from sys import stdin
-
-for _ in xrange(int(stdin.readline())):
-    num = int(stdin.readline())
-    print ' '.join(['!!!' + word + '!!!' if word.isupper() else word for word in stdin.readline().rstrip('\n').split()]).replace('.', '!!!')
