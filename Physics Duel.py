b3ef795c5cab0d136574f4dfaf5b882bb2a3661d
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 14:03:45 -0400

    Delete Physics Duel.py

--- Physics Duel.py
+++
@@ -1,8 +0,0 @@
-import sys, math
-
-h = 0
-for i in range(int(sys.stdin.readline())):
-    s, x, t = [float(_) for _ in sys.stdin.readline()[:-1].split()]
-    h += t * s * math.sin(x * math.pi / 180.0)
-
-print int(round((2 * 9.8 * h) ** 0.5))
