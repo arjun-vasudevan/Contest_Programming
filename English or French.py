4212b8cc7618b6f624d40e3f52f098657401a8f5
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 13:53:38 -0400

    Delete English or French.py

--- English or French.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
- 
-sCount, tCount = 0, 0
-
-for i in [stdin.readline() for _ in xrange(int(stdin.readline()))]:
-    sCount += i.count('s') + i.count('S')
-    tCount += i.count('t') + i.count('T')
-
-print 'French' if sCount >= tCount else 'English'
