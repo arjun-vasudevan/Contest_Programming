89f07786fd898673d36d1ccf8983270dd2c2d2fb
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 14:11:45 -0400

    Delete Alex's Bae.py

--- DMOJ/Alex's Bae.py
+++
@@ -1,7 +0,0 @@
-first = "".join(raw_input().lower().split())
-second = "".join(raw_input().lower().split())
-
-if len(first) == len(second) and sorted(first) == sorted(second):
-    print "yes"
-else:
-    print "no"
