3486ac872e53acbec5bb6d3a7ea4d449215e9535
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 13:58:37 -0400

    Delete Johnny and Garden.py

--- Johnny and Garden.py
+++
@@ -1,9 +0,0 @@
-name = raw_input()
-if "." not in name:
-    extension = raw_input()
-else:
-    extension = name[name.index(".") + 1:]
-    name = name[:name.index(".")]
-extension = extension.lower()
-
-print '"' + name + '"' + " - " + extension 
