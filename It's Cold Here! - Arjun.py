775510e60b300b541439d4a936c2b9204e521236
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 13:57:56 -0400

    Delete It's Cold Here! - Arjun.py

--- It's Cold Here! - Arjun.py
+++
@@ -1,9 +0,0 @@
-cities = {}
-while True:
-    both = raw_input().split()
-    cities[both[0]] = int(both[1])
-    if both[0] == "Waterloo":
-        break
-
-lst = sorted([(val, key) for key, val in cities.items()])
-print lst[0][1]
