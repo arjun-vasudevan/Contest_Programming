d29d8f0901dcbafcf1a55333bcb4187ca84eea7b
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 14:13:16 -0400

    Delete C.C. and Cheese-kun.py

--- DMOJ/C.C. and Cheese-kun.py
+++
@@ -1,8 +0,0 @@
-W, C = input(), input()
-
-if W == 3 and C >= 95:
-    print "C.C. is absolutely satisfied with her pizza."
-elif W == 1 and C <= 50:
-    print "C.C. is fairly satisfied with her pizza."
-else:
-    print "C.C. is very satisfied with her pizza."
