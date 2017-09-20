6c0c382514f02afebbb54123c56aedfefab4789a
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 13:57:47 -0400

    Delete Invitation Devastation.py

--- Invitation Devastation.py
+++
@@ -1,9 +0,0 @@
-template = raw_input("")
-numPeople = input("")
-names = []
-
-for i in range(numPeople):
-    names.append(raw_input(""))
-    
-for j in range(numPeople):
-    print template.replace(">", names[j])
