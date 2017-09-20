13dc137ad431cc82c62cca561c7706b5ad22dbe6
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 14:03:09 -0400

    Delete Party Invitation.py

--- Party Invitation.py
+++
@@ -1,7 +0,0 @@
-people = range(1, input() + 1)
-
-for i in [input() for x in range(input())]:
-    people = [item for index, item in enumerate(people) if (index + 1) % i != 0]
-
-for i in people:
-    print i
