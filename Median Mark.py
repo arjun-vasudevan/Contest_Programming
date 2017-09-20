bf936b6d67631767a4218594227e870d72974114
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 14:00:43 -0400

    Delete Median Mark.py

--- Median Mark.py
+++
@@ -1,9 +0,0 @@
-N = input()
-marks = [input() for x in range(N)]
-marks.sort()
-
-if N % 2 == 1:
-    print marks[N / 2]
-else:
-    median = (marks[N / 2] + marks[(N / 2) - 1]) / (2.0)
-    print int(round(median))
