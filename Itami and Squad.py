cf77bb6e62e21fa75ac58777be7bd89f2bf03484
Arjun Vasudevan <30818501+arjun-vasudevan@users.noreply.github.com>
2017-09-20 13:58:20 -0400

    Delete Itami and Squad.py

--- Itami and Squad.py
+++
@@ -1,5 +0,0 @@
-from sys import stdin
-
-N, L, R = map(int, stdin.readline().split())
-solds   = sorted(map(int, stdin.readline().split()), reverse = True)
-print sum(solds[i] for i in xrange(R - 1, N, L))
