1901266e9e8cb83892f501d8cd86f1dd8973070b
Arjun Vasudevan <30818501+arjunv2000@users.noreply.github.com>
2017-09-26 17:54:11 -0400

    Delete s.py

--- DMOJ-Python/s.py
+++
@@ -1,9 +0,0 @@
-antenna = input()
-eyes = input()
-
-if antenna >= 3 and eyes <= 4:
-    print "TroyMartian"
-if antenna <= 6 and eyes >= 2:
-    print "VladSaturnian"
-if antenna <= 2 and eyes <= 3:
-    print "GraemeMercurian"
