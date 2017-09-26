3da2fd64d4f5edd21ce83ae702db307b3bdc3b80
Arjun Vasudevan <30818501+arjunv2000@users.noreply.github.com>
2017-09-26 17:52:19 -0400

    Delete Writing the CCC.py

--- DMOJ-Python/Writing the CCC.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-
-types = {stdin.readline(): i + 1 for i in xrange(int(stdin.readline()))}
-
-probs = [(stdin.readline(), j + 1) for j in xrange(int(stdin.readline()))]
-probs.sort(key = lambda x: types[x[0]])
-
-for i in probs:
-    print i[1]
